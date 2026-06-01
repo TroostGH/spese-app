45a6409a415d989ec8a08cf0c92c0f2059095a81
TroostGH <davzan92platform@pm.me>
2026-06-01 07:21:11 +0200

    Bump cache version v3 -> v4 per nuovo export riepilogo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'spese-v3';
+var CACHE = 'spese-v4';
 var ASSETS = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', function(e) {
